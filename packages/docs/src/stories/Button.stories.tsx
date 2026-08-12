fc640f4a698256dd7268d200e0056c22bf6f5a94
Urassamx7 <marvin.lagas01234@gmail.com>
2026-08-12 13:40:43 +0200

    Refactor Button component to support multiple variants and improve styling. Update Storybook stories for better organization and add new examples. Modify package.json to include additional development dependencies for Storybook and Vitest.

--- packages/docs/src/stories/Button.stories.tsx
+++ packages/docs/src/stories/Button.stories.tsx
@@ -1,17 +1,65 @@
 import type { Meta, StoryObj } from '@storybook/react'
 import { Button, type ButtonProps } from '@urassamx7/react'
+import { ArrowRight } from 'phosphor-react'
 
 export default {
-	title: 'Button',
+	title: 'Form/Button',
 	component: Button,
 	args: {
-		children: 'Enviar',
+		children: 'Send',
+		disabled: false,
+	},
+	argTypes: {
+		variant: {
+			options: ['primary', 'secondary', 'tertiary', 'destructive'],
+			control: {
+				type: 'select',
+				labels: 'Primary',
+			},
+		},
 	},
 } as Meta<ButtonProps>
 
 export const Primary: StoryObj<ButtonProps> = {}
-export const Big: StoryObj<ButtonProps> = {
+export const Secondary: StoryObj<ButtonProps> = {
 	args: {
-		size: 'big',
+		variant: 'secondary',
+		children: 'Create new',
+	},
+}
+export const Tertiary: StoryObj<ButtonProps> = {
+	args: {
+		variant: 'tertiary',
+		children: 'Cancel',
+	},
+}
+export const Destructive: StoryObj<ButtonProps> = {
+	args: {
+		variant: 'destructive',
+		children: 'Delete',
+	},
+}
+
+export const Small: StoryObj<ButtonProps> = {
+	args: {
+		variant: 'primary',
+		size: 'sm',
+	},
+}
+
+export const WithIcon: StoryObj<ButtonProps> = {
+	args: {
+		variant: 'primary',
+		children: (
+			<>
+				Next step <ArrowRight weight='bold' />
+			</>
+		),
+	},
+}
+
+export const Disabled: StoryObj<ButtonProps> = {
+	args: {
+		disabled: true,
 	},
 }
--- packages/react/src/components/button.tsx
+++ packages/react/src/components/button.tsx
@@ -1,34 +1,108 @@
-import type { ComponentProps } from 'react'
+import type { ComponentProps, ElementType } from 'react'
 import { styled } from '../styles'
 
 export const Button = styled('button', {
+	all: 'unset',
+
+	borderRadius: '$sm',
+	fontSize: '$sm',
+	fontWeight: '$medium',
 	fontFamily: '$default',
-	backgroundColor: '$ignite300',
-	borderRadius: '$md',
-	padding: '$2 $4',
-	border: 0,
-	fontWeight: '$bold',
-	color: '$white',
+	textAlign: 'center',
+	minWidth: 120,
+	boxSizing: 'border-box',
+
+	display: 'flex',
+	alignItems: 'center',
+	justifyContent: 'center',
+
+	gap: '$2',
+	cursor: 'pointer',
+
+	transition: 'all 0.2s',
+
+	'&:disabled': {
+		cursor: 'not-allowed',
+	},
+	svg: {
+		width: '$4',
+		height: '$4',
+	},
+
 	variants: {
+		variant: {
+			primary: {
+				color: '$white',
+				backgroundColor: '$ignite500',
+
+				'&:not(:disabled):hover': {
+					backgroundColor: '$ignite300',
+				},
+				'&:disabled': {
+					backgroundColor: '$gray200',
+				},
+			},
+
+			secondary: {
+				color: '$ignite300',
+				backgroundColor: 'transparent',
+				border: '2px solid $ignite500',
+
+				'&:not(:disabled):hover': {
+					backgroundColor: '$ignite500',
+					color: '$white',
+				},
+				'&:disabled': {
+					color: '$gray200',
+					borderColor: '$gray200',
+				},
+			},
+
+			tertiary: {
+				color: '$gray200',
+				backgroundColor: 'transparent',
+				border: 'none',
+
+				'&:not(:disabled):hover': {
+					color: '$gray100',
+				},
+				'&:disabled': {
+					color: '$gray600',
+				},
+			},
+
+			destructive: {
+				color: '$white',
+				backgroundColor: '$red500',
+				border: '2px solid $red500',
+
+				'&:not(:disabled):hover': {
+					backgroundColor: '$red700',
+				},
+				'&:disabled': {
+					backgroundColor: '$red200',
+				},
+			},
+		},
+
 		size: {
-			default: {
-				fontSize: 14,
-				padding: '$2 $4',
+			md: {
+				padding: '0 $4',
+				height: 46,
 			},
-			small: {
-				fontSize: 12,
-				padding: '$1 $2',
-			},
-			big: {
-				fontSize: 16,
-				padding: '$3 $6',
+			sm: {
+				padding: '0 $2',
+				height: 38,
 			},
 		},
 	},
 
 	defaultVariants: {
-		size: 'default',
+		variant: 'primary',
+		size: 'md',
 	},
 })
 
-export type ButtonProps = ComponentProps<typeof Button>
+export type ButtonProps = ComponentProps<typeof Button> & {
+	as?: ElementType
+}
