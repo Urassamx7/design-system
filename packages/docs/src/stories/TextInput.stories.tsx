16d1ef36c4a1f33b177f13da2653e9b32a4cf91d
Urassamx7 <marvin.lagas01234@gmail.com>
2026-08-12 14:12:33 +0200

    Update index.tsx to export TextInput component alongside Text for improved modularity and component accessibility.

---
+++ packages/docs/src/stories/TextInput.stories.tsx
@@ -0,0 +1,43 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Box, Text, TextInput, type TextInputProps } from '@urassamx7/react'
+
+export default {
+	title: 'Form/Text Input',
+	component: TextInput,
+	args: {},
+	decorators: [
+		(story) => {
+			return (
+				<Box
+					as='label'
+					css={{
+						display: 'flex',
+						flexDirection: 'column',
+						gap: '$2',
+					}}
+				>
+					<Text size='sm'>Email address</Text>
+					{story()}
+				</Box>
+			)
+		},
+	],
+} as Meta<TextInputProps>
+
+export const Primary: StoryObj<TextInputProps> = {
+	args: {
+		placeholder: 'Type your name',
+	},
+}
+export const WithPrefix: StoryObj<TextInputProps> = {
+	args: {
+		prefix: 'cal.com/',
+	},
+}
+
+export const Disabled: StoryObj<TextInputProps> = {
+	args: {
+		disabled: true,
+		placeholder: 'Type your name',
+	},
+}
